b8fb7c4d2ab1686aec0148e4dbe7108be1ad3e75
mihaelastoychev <>
2021-11-04 17:09:07 +0000

    test2

--- src/exercise4/MinIndex.java
+++ src/exercise4/MinIndex.java
@@ -6,20 +6,21 @@ import java.util.Arrays;
 public class MinIndex {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        double number = 1234;
+        int number = 0;
+        double min = 12345;
         //System.out.println("Enter the size of array: ");
         int size = input.nextInt();
 
         double[] array = new double[size];
         //System.out.println("Enter numbers: ");
 
-        for (int index = 0; index < size; index++){
+        for (int index = 0; index < size; index++) {
             array[index] = input.nextInt();
-            if (array[index] < number){
-                number = array[index];
+            if (min > array[index]) {
+                min = array[index];
+                number = index;
             }
         }
-        System.out.println(number);
+        System.out.println("Min index: " + number);
     }
 }
